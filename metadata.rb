4524d0b6122cb2cc93dcbd7ef67e98e4f9b6c4b3
James Cuzella <james.cuzella@lyraphase.com>
2013-12-30 07:23:56 -0700

    Use Berkshelf-ish VERSION file, and fill in some extra metadata
    Note: see https://github.com/RallySoftware-cookbooks/cookbook-development/blob/abf7b136965eeab966e222f8de4f0fd69dfafe0e/lib/cookbook/development/rake/version_tasks.rb#L17-21

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,14 @@ maintainer       "Chris Peplin"
 maintainer_email "chris.peplin@rhubarbtech.com"
 license          "Apache 2.0"
 description      "Installs/configures MythTV packages"
-version          "0.0.2"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
+
+recipe 'mythtv', 'Installs/Configures mythtv repos'
+recipe 'frontend', 'Installs/Configures mythfrontend'
+recipe 'backend', 'Installs/Configures mythbackend'
+
+supports 'ubuntu'
 
 depends "mysql"
 depends "apt"
