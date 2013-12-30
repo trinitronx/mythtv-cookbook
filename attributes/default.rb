33e33e0a25111d6ea1976e9d263a6e8fbc2a3d82
James Cuzella <james.cuzella@lyraphase.com>
2013-12-30 06:31:09 -0700

    Intelligently detect the latest version of MythbuntuPPA/mythtv to use based on LSB codename

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+default[:mythtv][:version] = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,22 @@
-execute "add-apt-repository ppa:mythbuntu/0.24"
+case node[:platform_family]
+when "rhel"
+  return
+when "debian"
+  include_recipe 'apt'
+  
+  if MythbuntuPPA.supports_version?(node[:lsb][:codename], node[:mythtv][:version])
+    mythbuntu_version = node[:mythtv][:version]
+  else
+    mythbuntu_version = MythbuntuPPA.latest_supported_version(node[:lsb][:codename])
+  end
+
+  apt_repository 'mythbuntu-ppa' do
+    uri "http://ppa.launchpad.net/mythbuntu/#{mythbuntu_version}/ubuntu"
+    distribution node[:lsb][:codename]
+    components [:main]
+    keyserver 'keyserver.ubuntu.com'
+    key '0x1504888C'
+    deb_src true
+  end
+
+end
