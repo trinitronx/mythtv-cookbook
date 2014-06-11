dc58ec699e86cbc4eccdc210f05f4091e633c53e
James Cuzella <james.cuzella@lyraphase.com>
2014-06-10 21:11:43 -0600

    fixup! 9c90b03

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
 default[:mythtv][:version] = nil
+default[:mythtv][:raop_key][:url] = 'https://raw.github.com/juhovh/shairplay/master/airport.key'
+default[:mythtv][:raop_key][:checksum] = '7a07afcdf4cbb5653318a329906afe5367cb8b13'
